dee9c7b2192f673395f5ef3e0b66ab4cbaef0b4f
benmerigen <benmerigen@gmail.com>
2024-01-01 11:30:59 +0200

    Add tests for songs rating

--- logic_layer/songs_logic.py
+++ logic_layer/songs_logic.py
@@ -1,6 +1,7 @@
 import uuid
 
 from infrastructure_layer import api
+from logic_layer.users_logic import create_user
 
 
 def create_song(song_genre, song_performer, song_title, song_year):
@@ -25,6 +26,38 @@ def add_upvote(playlistName_NotRelevant, test_song_title, test_username, test_pa
         'user_password': test_password
     }
     return api.put("/songs/upvote", payload)
+
+def down_vote(playlistName_NotRelevant, test_song_title, test_username, test_password):
+    payload = {
+        'playlist_name': playlistName_NotRelevant,
+        'song_title': test_song_title,
+        'user_name': test_username,
+        'user_password': test_password
+    }
+    return api.put("/songs/downvote", payload)
+
+
+# build 3 songs and user and voting them
+def build_songs_for_test():
+    create_user("Arnold", "topsicrt") # create user for voting
+    for i in range(1,6): # create 5 songs
+        create_song("test_song_genre", "test_song_performer", f"test_song_title_{i}", 1990)
+    song_num = 1
+    for j in range(13, 22, 2): # add votes to the song (13, 15, 17)
+        add_vote(j, "not_relevant", f"test_song_title_{song_num}", "Arnold", "topsicrt")
+        song_num += 1
+
+# temp_function for build songs
+def add_vote(numberOfVotes, playlist_name, song_title, user_name, user_password): # temp_function for build songs
+    for i in range (numberOfVotes):
+        add_upvote(playlist_name, song_title, user_name, user_password)
+
+def search_for_songs (rank, op):
+    params = {
+        "rank": rank,
+        "op": op
+    }
+    return api.get("/songs/ranked_songs", params)
 
 def delete_db_songs_data():
     return api.delete("/admin/delete_all_songs")
--- test_layer/test_songs.py
+++ test_layer/test_songs.py
@@ -1,6 +1,6 @@
 import pytest
 
-from logic_layer.songs_logic import create_song, get_song, add_upvote
+from logic_layer.songs_logic import create_song, get_song, add_upvote, build_songs_for_test, search_for_songs, down_vote
 from logic_layer.users_logic import create_user
 
 
@@ -57,4 +57,51 @@ def test_upvote_song(set_up_data_base):
     create_upvote_response = add_upvote("playlistName_NotRelevant", "test_song_title","test_username", "test_password")
     assert create_upvote_response.status_code == 200, "Can`t up vote to this song"
     assert create_upvote_response.json()['data']['rating'] == 1
+
+# Test for successfully downvote
+def test_downvote_song(set_up_data_base):
+    create_song("test_song_genre", "test_song_performer", "test_song_title", 1990)
+    create_user("test_username", "test_password")
+    add_upvote("playlistName_NotRelevant", "test_song_title", "test_username", "test_password")
+    # already checked that 3 actions above
+
+    create_downvote_response = down_vote("playlistName_NotRelevant", "test_song_title", "test_username", "test_password")
+    assert create_downvote_response.status_code == 200, "Can`t down vote to this song"
+    assert create_downvote_response.json()['data']['rating'] == 0 # the down vote happened
+
+# Test for downvote do not down under 0
+def test_check_downvote_zero(set_up_data_base):
+    create_song("test_song_genre", "test_song_performer", "test_song_title", 1990)
+    create_user("test_username", "test_password")
+    for i in range(4):
+        create_downvote_response= down_vote("playlistName_NotRelevant", "test_song_title", "test_username","test_password")
+    assert create_downvote_response.json()['data']['rating'] == 0  # the rating is still zero
+
+
+
+
+
+
+
+# Test to get songs with low\high\equal rating than x
+def test_search_by_rating(set_up_data_base):
+    build_songs_for_test()
+    rating = 17 # rating for filter
+    less_rating_response = search_for_songs(rating, "less")
+    for i in range(1,3):
+        searh_by_rating(f"test_song_title_{i}", less_rating_response.json()['data'])
+
+    eq_rating_response = search_for_songs(rating, "eq")
+    assert "test_song_title_3" in eq_rating_response.json()['data']
+
+    greater_rating_response = search_for_songs(rating, "greater")
+    for i in range(4,6):
+        searh_by_rating(f"test_song_title_{i}", greater_rating_response.json()['data'])
+
+def searh_by_rating(song_tile, data):
+    assert song_tile in data
+
+
+
+
 
