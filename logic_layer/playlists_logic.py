89798e8436c9ad9ada774b991eea22ba812f0358
benmerigen <benmerigen@gmail.com>
2024-01-01 15:42:33 +0200

    Add Test for playlist alpi

--- logic_layer/playlists_logic.py
+++ logic_layer/playlists_logic.py
@@ -2,16 +2,16 @@ from infrastructure_layer import api
 
 
 # Test for successfully add song to user`s playlist
-def new_playlist_payload(user_name, user_password, song_title):
+def new_playlist_payload(playlist_name, song_title, user_name, user_password):
     return {
-        "playlist_name": "The best of the best of the best",
+        "playlist_name": playlist_name,
         "song_title": song_title,
         "user_name": user_name,
         "user_password": user_password
     }
 
-def add_song_to_playlist(user_name, user_password, song_title):
-    payload = new_playlist_payload(user_name, user_password, song_title)
+def add_song_to_playlist(playlist_name, song_title, user_name, user_password):
+    payload = new_playlist_payload(playlist_name, song_title, user_name, user_password)
     return api.post("/playlists/add_song", payload)
 
 
